9d7e58214cb241a125baaeb8c01b3e7cefdf6408
Denis-Kucherian <denisska.tlt@gmail.com>
2023-03-16 15:24:55 +0400

    homework7

--- example.py
+++ example.py
@@ -6,7 +6,7 @@
 import math
 
 
-class R_trapezoid:
+class Trapezoid:
     def __init__(self, A, B, C, D):
         # вычисляем длину стороны по координатам точек
         def side_length(point1, point2):
@@ -21,25 +21,30 @@ class R_trapezoid:
         self.CD = side_length(self.C, self.D)   # длина стороны CD
         self.DA = side_length(self.D, self.A)   # длина стороны DA
 
-    # проверка, является ли фигура равнобочной трапецией
-
+    # проверяем, является ли фигура равнобокой трапецией, а не простой трапецией или параллерограммом или треугольником
+    def check_trapezoid(self):
+        return (self.AB == self.CD) and (self.BC != self.DA) and (self.B != self.C)
 
     # вычисляем периметр
-    def perimeter_triangle(self):
-        return self.AB + self.BC + self.CA
+    def perimeter_trapezoid(self):
+        return self.AB + self.BC + self.CD + self.DA
 
-    # вычисляем площадь треугольника по формуле Герона
-    def area_triangle(self):
-        p_2 = self.perimeter_triangle() / 2
-        return math.sqrt(p_2 * (p_2 - self.AB) * (p_2 - self.BC) * (p_2 - self.CA))
-
-    # вычисляем высоту
-    def height_triangle(self):
-        return self.area_triangle() / self.AB
+    # вычисляем площадь трапеции по формуле Брахмапутры
+    def area_trapezoid(self):
+        p_2 = self.perimeter_trapezoid() / 2    # полупериметр
+        return math.sqrt((p_2 - self.DA) * (p_2 - self.BC) * (p_2 - self.AB) ** 2)
 
 
-# tr = Triangle((5, 5), (6, 6), (9, 15))
-#
-# print(tr.perimeter_triangle())
-# print(tr.area_triangle())
-# print(tr.height_triangle())
+tr = Trapezoid((2, 2), (4, 6), (6, 6), (8, 2))  # равнобокая трапеция
+
+if tr.check_trapezoid():
+    print("Это равнобокая трапеция")
+    print(f"Сторона AB = {tr.AB}")
+    print(f"Сторона BC = {tr.BC}")
+    print(f"Сторона CD = {tr.CD}")
+    print(f"Сторона DA = {tr.DA}")
+    print(f"Периметр трапеции = {tr.perimeter_trapezoid()}")
+    print(f"Площадь трапеции = {tr.area_trapezoid()}")
+else:
+    print("Это не равнобокая трапеция, проверь координаты")
+
